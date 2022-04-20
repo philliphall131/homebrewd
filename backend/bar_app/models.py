93442170ee215ca81213b769915c0927b632f71b
philliphall131 <phillip.hall131@gmail.com>
2022-04-20 10:12:14 -0500

    some morning work

--- backend/bar_app/models.py
+++ backend/bar_app/models.py
@@ -27,14 +27,17 @@ class Bar(models.Model):
 
 class Beer(models.Model):
     bar = models.ForeignKey(Bar, on_delete=models.CASCADE, related_name="beers")
-    tap = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(12)])
-    batch_id = models.CharField(max_length=255) # this will store the batch id from BF
+    tap = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(12)])
+    batch_id = models.CharField(max_length=255, null=True, blank=True) # this will store the batch id from BF
     is_finished = models.BooleanField(default=False)
     date_added = models.DateField(auto_now=False, auto_now_add=True)
     date_finished = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
-    quantity_start = models.IntegerField(validators=[MinValueValidator(0)]) #quantity in oz
-    quantity_remaining = models.IntegerField(validators=[MinValueValidator(0)])
-    rating = models.DecimalField(max_digits=2, decimal_places=1)
+    quantity_start = models.IntegerField(default=640, validators=[MinValueValidator(0)]) #quantity in oz
+    # quantity_remaining = self.initial_remaining()
+    rating = models.DecimalField(null=True, blank=True, max_digits=2, decimal_places=1)
 
     def __str__(self):
         return f"{self.name}, Tap {self.tap}"
+
+    def initial_remaining(self):
+        pass
